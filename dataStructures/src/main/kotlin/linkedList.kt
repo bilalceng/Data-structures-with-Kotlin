8219d982ce0f34b1d8fe5ffd55f9601aa1b09192
bilalceng <bilalberekgm@gmail.com>
2023-08-04 17:24:55 +0300

    applying append method

--- dataStructures/src/main/kotlin/linkedList.kt
+++ dataStructures/src/main/kotlin/linkedList.kt
@@ -7,6 +7,12 @@ data class Node <T>(val value: T, var next: Node<T>? = null ){
             "$value"
         }
     }
+    companion object {
+        fun <T> createNode(value: T, next: Node<T>? = null): Node<T> {
+            return Node(value, next)
+        }
+    }
+
 }
 
 class LinkedList<T>{
@@ -38,7 +44,45 @@ class LinkedList<T>{
 
         return this
     }
+
+    fun append(value : T): LinkedList<T>{
+        if(isEmpty()){
+            push(value)
+            return this
+        }
+        tail?.next = Node(value)
+        tail = tail?.next
+        size ++
+        return this
+    }
+
+    fun insertAt(index: Int, value: T): Node<T>{
+        var currentIndex = 0
+        var currentNode = head
+
+        while(currentNode != null && currentIndex < index){
+            currentNode = currentNode.next
+            currentIndex++
+        }
+        var newNode = Node.createNode(value)
+
+
+        if(currentNode == tail){
+            append(value)
+            return tail!!
+        }
+
+        newNode.next = currentNode?.next
+        currentNode?.next = newNode
+
+        size++
+        return Node(value)
+
+    }
+
 }
+
+
 
 fun main(){
 
@@ -82,6 +126,35 @@ fun main(){
         list.push(node1).push(node2).push(node3)
 
         println(list)
+    }
+
+    "append" example{
+        val node1 = Node("one")
+        val node2 = Node("two")
+        val node3 = Node("three")
+
+        var linkedList = LinkedList<Node<String>>()
+        linkedList.append(node1).append(node2).append(node3)
+        println(linkedList)
+    }
+
+    "inserting certain position" example {
+        var list = LinkedList<Node<Int>>()
+
+        val node1 = Node<Int>(1)
+        val node2 = Node<Int>(2)
+        val node3 = Node<Int>(3)
+
+        /*list.push(node1)
+        list.push(node2)
+        list.push(node3)*/
+
+        for (i in 1..3){
+            list.insertAt(1,Node.createNode(4*i))
+        }
+
+        println(list)
+
     }
 
 
